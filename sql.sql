b55254a0a15e46a68d3b84c27615ca4f82097222
dbokovoy <72661089+dbokovoy@users.noreply.github.com>
2025-01-14 11:52:41 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -298,3 +298,30 @@ FROM y_11
 JOIN y_12 ON y_11.country = y_12.country
 JOIN y_13 ON y_12.country = y_13.country
 ORDER BY y_2011 DESC;
+
+/* Подзапросы во FROM
+/* Найдите средние значения полей, в которых указаны минимальная и максимальная длительность отобранных фильмов. Отобразите только эти два поля. Назовите их avg_min_length и avg_max_length соответственно.
+select avg(top2.min_length) as avg_min_length,
+       avg(top2.max_length) as avg_max_length
+from
+(SELECT 
+    top.rating,
+    MIN(top.length) AS min_length,
+    MAX(top.length) AS max_length,
+    AVG(top.length) AS avg_length,
+    MIN(top.rental_rate) AS min_rental_rate,
+    MAX(top.rental_rate) AS max_rental_rate,
+    AVG(top.rental_rate) AS avg_rental_rate
+FROM (
+    SELECT 
+        title,
+        rental_rate,
+        length,
+        rating
+    FROM movie
+    WHERE rental_rate > 2
+    ORDER BY length DESC
+    LIMIT 40
+) AS top
+GROUP BY top.rating
+ORDER BY avg_length) as top2
